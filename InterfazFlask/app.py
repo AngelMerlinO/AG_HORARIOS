20593f2457e328c268ee158436f011a6403fa6df
Jose Angel Ortega Merlin <angelortegamerlin@gmail.com>
2024-07-21 22:11:14 -0600

    update de proyecto

--- InterfazFlask/app.py
+++ InterfazFlask/app.py
@@ -17,14 +17,17 @@ class Materia:
             'viernes': viernes
         }
 
+# Importar mapa_curricular de data.py
 data_path = os.path.join(os.path.dirname(__file__), '../data.py')
 data = {}
 exec(open(data_path).read(), data)
 
 mapa_curricular = data['mapa_curricular']
 
+# Lista para almacenar las materias seleccionadas
 selected_materias = []
 
+# Variable global para el cuatrimestre del alumno
 cuatrimestre_alumno = None
 
 @app.route('/', methods=['GET', 'POST'])
@@ -41,31 +44,32 @@ def materias():
     global cuatrimestre_alumno
     if cuatrimestre_alumno is None:
         return redirect(url_for('index'))
-    
+
+    error_message = None
+
     if request.method == 'POST':
         materia_nombre = request.form['materia']
         grupo = request.form['grupo'].upper()
+        
         horarios = {
-            'lunes': [int(h) for h in request.form.getlist('lunes')[0].split(',') if h],
-            'martes': [int(h) for h in request.form.getlist('martes')[0].split(',') if h],
-            'miercoles': [int(h) for h in request.form.getlist('miercoles')[0].split(',') if h],
-            'jueves': [int(h) for h in request.form.getlist('jueves')[0].split(',') if h],
-            'viernes': [int(h) for h in request.form.getlist('viernes')[0].split(',') if h]
+            'lunes': [int(h) for h in request.form.get('lunes', '').split(',') if h],
+            'martes': [int(h) for h in request.form.get('martes', '').split(',') if h],
+            'miercoles': [int(h) for h in request.form.get('miercoles', '').split(',') if h],
+            'jueves': [int(h) for h in request.form.get('jueves', '').split(',') if h],
+            'viernes': [int(h) for h in request.form.get('viernes', '').split(',') if h]
         }
 
         cuatrimestre_materia = next((m['cuatrimestre'] for m in mapa_curricular if m['nombre'] == materia_nombre), None)
         if cuatrimestre_materia is None:
-            return "La materia seleccionada no existe en el mapa curricular."
-
-        selected_materia = f"{materia_nombre} - {grupo}"
-        
-        if selected_materia not in [f"{m.nombre} - {m.grupo}" for m in selected_materias]:
-            new_materia = Materia(materia_nombre, cuatrimestre_materia, grupo, horarios['lunes'], horarios['martes'], horarios['miercoles'], horarios['jueves'], horarios['viernes'])
-            selected_materias.append(new_materia)
+            error_message = "La materia seleccionada no existe en el mapa curricular."
         else:
-            return "La materia ya ha sido seleccionada."
-        
-        return redirect(url_for('materias'))
+            selected_materia = f"{materia_nombre} - {grupo}"
+            
+            if selected_materia not in [f"{m.nombre} - {m.grupo}" for m in selected_materias]:
+                new_materia = Materia(materia_nombre, cuatrimestre_materia, grupo, horarios['lunes'], horarios['martes'], horarios['miercoles'], horarios['jueves'], horarios['viernes'])
+                selected_materias.append(new_materia)
+            else:
+                error_message = "La materia ya ha sido seleccionada."
 
     json_data = {
         'cuatrimestre_alumno': cuatrimestre_alumno,
@@ -83,7 +87,16 @@ def materias():
 
     json_string = json.dumps(json_data, default=str)
 
-    return render_template('index.html', cuatrimestre_alumno=cuatrimestre_alumno, mapa_curricular=mapa_curricular, selected_materias=selected_materias, materias_json=json_string)
+    return render_template('index.html', cuatrimestre_alumno=cuatrimestre_alumno, mapa_curricular=mapa_curricular, selected_materias=selected_materias, materias_json=json_string, error_message=error_message)
+
+@app.route('/eliminar_materia', methods=['POST'])
+def eliminar_materia():
+    global selected_materias
+    data = request.get_json()
+    nombre = data['nombre']
+    grupo = data['grupo']
+    selected_materias = [m for m in selected_materias if not (m.nombre == nombre and m.grupo == grupo)]
+    return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
     app.run(debug=True)
